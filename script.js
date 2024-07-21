e41f6cd14691b71da2ac2e23f06abb37d5929365
HwangPham <163698523+HwangPham@users.noreply.github.com>
2024-07-21 18:06:21 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function selectOption(option) {
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'Cho chọn lại '; 
+        document.getElementById('no-button').innerText = 'Cho chọn lại 😤'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
